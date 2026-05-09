e402d4777a889921899ac54980a21bccddfc3b79
RibZu <109860001+RibZu@users.noreply.github.com>
2026-05-08 21:43:59 -0300

    Revert "Readme"
    This reverts commit 4cf8a8faed717277c753b147a4d72157dda944ea.

--- Riberi-Simon-Paractico-Parte1/modelos/Historial.php
+++
@@ -1,7 +0,0 @@
-<?php
-function registrarHistorial($conexion, $noticia_id, $nombre_usuario, $accion) {
-    $query = "INSERT INTO historial (noticia_id, nombre_usuario, accion_realizada, fecha_hora) 
-              VALUES ($noticia_id, '$nombre_usuario', '$accion', NOW())";
-    mysqli_query($conexion, $query);
-}
-?>
